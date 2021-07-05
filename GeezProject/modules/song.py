3634d48a661b614fe1ac6355985a200e497fc63b
petrik8 <83970295+petrik8@users.noreply.github.com>
2021-07-05 15:00:59 +0700

    Update song.py

--- GeezProject/modules/song.py
+++ GeezProject/modules/song.py
@@ -50,7 +50,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 **mencari lagu...**")
+    m = message.reply("🔎 mencari lagu...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -67,16 +67,16 @@ def song(client, message):
         results[0]["views"]
 
     except Exception as e:
-        m.edit("Lagu tidak ditemukan.\n\nCoba masukan judul lagu yang lebih jelas.")
+        m.edit("Lagu tidak ditemukan.\n\nCoba masukan judul lagu dengan benar.")
         print(str(e))
         return
-    m.edit("⬇️ **sedang mendownload...**")
+    m.edit("🗳 sedang mendownload...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "Diupload oleh **Lagu Kamu**"
+        rep = "💿 Diupload oleh **Lagu Kamu**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
